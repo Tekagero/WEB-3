1c4c92a4c4d489b4c287dbf3e01eb0eb48b1e061
Дементьев Александр Владимирович <80294078+Tekagero@users.noreply.github.com>
2022-01-31 11:18:59 +0300

    Add files via upload

---
+++ parts/header.php
@@ -0,0 +1,9 @@
+<header class="header">
+    <div class="headerName">
+        <a class="hrefNameSite" href="/Laba3">Сайт объявлений</a>
+    </div>
+    <div class="headerButton">
+        <button class="headerSignInButton">Войти</button>
+        <button class="headerSignUpButton">Регистрация</button>
+    </div>
+</header>
